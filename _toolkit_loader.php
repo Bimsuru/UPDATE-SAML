ab661efe52e62f1cf13de33e1dbbf95af5af0ef6
Bimsuru <47709078+Bimsuru@users.noreply.github.com>
2020-04-30 21:09:42 +0530

    commit 6

--- _toolkit_loader.php
+++ _toolkit_loader.php
@@ -8,14 +8,14 @@ $libDir = __DIR__ . '/lib/Saml2/';
 $extlibDir = __DIR__ . '/extlib/';
 
 // Load composer
-if (file_exists(__DIR__ .'/vendor/autoload.php')) {
+if (file_exists(__DIR__ .'/vendor/autoload.php') == true) {
     require __DIR__ . '/vendor/autoload.php';
 }
 
 // Load now external libs
 require_once $extlibDir . 'xmlseclibs/xmlseclibs.php';
 
-$folderInfo = scandir($libDir);
+$folderInfo = scandir(__DIR__ .$libDir);
 
 foreach ($folderInfo as $element) {
     if (is_file($libDir.$element) && (substr($element, -4) === '.php')) {
